546bde7b89ce2ca3088d53f28cb22067b23bfe0f
ViniciusNevesSalamene <150290843+ViniciusNevesSalamene@users.noreply.github.com>
2024-01-30 23:08:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function recarregar(){
+    fetch('html.txt')
+    .then(response => response.text())
+    .then(text => {
+        const array = text.split("\n");
+        message = array[0]
+        var count = `<b>${parseInt(array[1])}</b>`
+        counter.innerHTML = message
+        num.innerHTML = count
+    })
+}
+recarregar()
+var counter = document.getElementById('counter')
+var message = ''
+var num = document.getElementById('num')
